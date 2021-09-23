f7dd60c5c56281232140e2fd45d7f0f4710cbf06
Idrek <iddarg@gmail.com>
2021-09-23 22:24:05 +0200

    New `parseCommandLine` function

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,6 +1,17 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿module App.Program
 
-open System
+open CommandLine
+type CLIResult = 
+    | Success = 0
+    | CommandLineParseError = 1
+    | CommandLineNotParsed = 2
+
+let parseCommandLine (argv: array<string>) : Result<Parsed<Options>, CLIResult> =
+    let commandLine = CommandLine.Parser.Default.ParseArguments<Options>(argv)
+    match commandLine with
+    | :? CommandLine.NotParsed<Options> -> Result.Error CLIResult.CommandLineNotParsed
+    | :? CommandLine.Parsed<Options> as parsed -> Ok parsed
+    | _ -> Result.Error CLIResult.CommandLineParseError
 
 [<EntryPoint>]
 let main argv =
