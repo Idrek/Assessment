d49471cf56766f2be3ec333e0b4b8262c898c620
Idrek <iddarg@gmail.com>
2021-09-23 12:54:15 +0200

    New `runRequest` function

--- src/App/Http.fs
+++ src/App/Http.fs
@@ -1,10 +1,23 @@
 module App.Http
 
+open Hopac
+
 module C = HttpFs.Client
 module MRequest = C.Request
+module MResponse = C.Response
 
 type TRequest = C.Request
 
 let createRequest (uri: string) : TRequest =
     MRequest.createUrl (C.HttpMethod.Get) uri
-    
+
+let runRequest (request: TRequest) : Async<Result<string, string>> =
+    job {
+        try
+            use! response = C.getResponse request
+            let! body = MResponse.readBodyAsString response
+            return Ok body
+        with 
+            | ex -> return Result.Error ex.Message
+    } |> Job.toAsync
+
