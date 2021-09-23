0a22c07e90b7e67b21e73b2472fd8c8d773e85bb
Idrek <iddarg@gmail.com>
2021-09-23 19:52:58 +0200

    New `Assets.Resume` method

--- src/App/Assets.fs
+++ src/App/Assets.fs
@@ -1,16 +1,23 @@
 namespace App.Assets
 
+open System
+
 module FS = App.FileSystem
 module Json = App.Json
 
 type File = System.IO.File
+type FileAccess = System.IO.FileAccess
+type FileMode = System.IO.FileMode
 type FileStream = System.IO.FileStream
 type HttpClient = System.Net.Http.HttpClient
+type HttpMethod = System.Net.Http.HttpMethod
+type HttpRequestMessage = System.Net.Http.HttpRequestMessage
 type IHttpClientFactory = System.Net.Http.IHttpClientFactory
 type JsonDocument = System.Text.Json.JsonDocument
 type JsonElement = System.Text.Json.JsonElement
 type JsonProperty = System.Text.Json.JsonProperty
 type Queue<'t> = System.Collections.Generic.Queue<'t>
+type RangeHeaderValue = System.Net.Http.Headers.RangeHeaderValue
 type Stack<'t> = System.Collections.Generic.Stack<'t>
 type String = System.String
 
@@ -114,4 +121,34 @@ type Assets (clientFactory: IHttpClientFactory) =
                 with
                     | ex -> return Result.Error ex.Message
             }
+
+        member this.Resume (uri: string) (localPath: string) : Async<Result<unit, string>> =
+            let client : HttpClient = clientFactory.CreateClient("Assets")
+            let fileStream : FileStream = new FileStream(localPath, FileMode.Append, FileAccess.Write)
+            let mutable count : int64 = FS.countBytes localPath
+            async {
+                let buffer : array<byte> = Array.zeroCreate 4096
+                let mutable isEnd : bool = false
+                try
+                    while not isEnd do
+                        let range : RangeHeaderValue = 
+                            RangeHeaderValue(Nullable<int64>(count), Nullable<int64>(count + 4096L))
+                        client.DefaultRequestHeaders.Range <- range
+                        use httpRequestMessage : HttpRequestMessage = new HttpRequestMessage(HttpMethod.Get, Uri(uri))
+                        use! response = client.SendAsync(httpRequestMessage) |> Async.AwaitTask
+                        use! responseStream = response.Content.ReadAsStreamAsync() |> Async.AwaitTask
+                        let read = responseStream.Read(buffer, 0, 4096)
+                        if read > 0
+                        then
+                            let bufferByteOffset : int = 0
+                            fileStream.Write(buffer, bufferByteOffset, read)
+                            fileStream.Flush()
+                            count <- count + int64 read
+                        else isEnd <- true
+                    fileStream.Close()
+                    return Ok ()
+                with
+                    | ex -> return Result.Error ex.Message
+            }
+
 
