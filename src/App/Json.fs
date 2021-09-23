507c4afadbefff88177acffa87787e64fdb9262b
Idrek <iddarg@gmail.com>
2021-09-23 11:32:01 +0200

    New `objectChilds` function

--- src/App/Json.fs
+++ src/App/Json.fs
@@ -7,4 +7,9 @@ let arrayChilds (property: JsonProperty) : Result<seq<JsonElement>, string> =
     try
         property.Value.EnumerateArray() |> Seq.cast<JsonElement> |> Ok
     with | ex -> Result.Error ex.Message
+
+let objectChilds (item: JsonElement) : Result<seq<JsonProperty>, string> =
+    try
+        item.EnumerateObject() |> Seq.cast<JsonProperty> |> Ok
+    with | ex -> Result.Error ex.Message
     
