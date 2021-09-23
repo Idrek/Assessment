32ab0efcf5b44fe13d895eae27e2bd0da8069ee8
Idrek <iddarg@gmail.com>
2021-09-23 10:39:22 +0200

    New `buildFullPath` function

--- src/App/FileSystem.fs
+++ src/App/FileSystem.fs
@@ -1,6 +1,8 @@
 module App.FileSystem
 
 type Directory = System.IO.Directory
+type DirectoryInfo = System.IO.DirectoryInfo
+type Path = System.IO.Path
 
 let createDir (path: string) : Option<string> =
     try Directory.CreateDirectory path |> ignore; None
@@ -10,4 +12,9 @@ let ensureDir (path: string) : Option<string> =
     if Directory.Exists path
     then None
     else createDir path
+
+let buildFullPath (baseDir: string) (path: string) : Result<string, string> =
+    try
+        Ok <| DirectoryInfo(Path.Join(baseDir, path)).FullName
+    with | ex -> Result.Error ex.Message
     
