0484ddbabbf5cb32d7450176e763fbbc804a7804
Idrek <iddarg@gmail.com>
2021-09-23 10:37:50 +0200

    New `createDir` function

--- src/App/FileSystem.fs
+++ src/App/FileSystem.fs
@@ -1,2 +1,8 @@
 module App.FileSystem
 
+type Directory = System.IO.Directory
+
+let createDir (path: string) : Option<string> =
+    try Directory.CreateDirectory path |> ignore; None
+    with | ex -> Some ex.Message
+    
