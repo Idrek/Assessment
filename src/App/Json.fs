3f632ca58a4344492e58cd2511ef0f14fcd855d9
Idrek <iddarg@gmail.com>
2021-09-23 11:31:21 +0200

    New `arrayChilds` function

--- src/App/Json.fs
+++ src/App/Json.fs
@@ -1,1 +1,10 @@
 module App.Json
+
+type JsonElement = System.Text.Json.JsonElement
+type JsonProperty = System.Text.Json.JsonProperty
+
+let arrayChilds (property: JsonProperty) : Result<seq<JsonElement>, string> =
+    try
+        property.Value.EnumerateArray() |> Seq.cast<JsonElement> |> Ok
+    with | ex -> Result.Error ex.Message
+    
