7d64538f43ae4e75ced7a4a9fab0b902cbece747
Idrek <iddarg@gmail.com>
2021-09-23 23:05:06 +0200

    Add tests for `fromBytes` function

--- test/AppTest/HexadecimalTests.fs
+++ test/AppTest/HexadecimalTests.fs
@@ -11,4 +11,10 @@ let ``Test fromByte function`` () =
     Assert.Equal("0F", fromByte Case.Upper 15uy)
     Assert.Equal("FF", fromByte Case.Upper 255uy)
 
+[<Fact>]
+let ``Test fromBytes function`` () =
+    let empty : array<byte> = Array.empty
+    Assert.Equal(String.Empty, fromBytes Case.Upper empty)
+    Assert.Equal("00090A0FFF", fromBytes Case.Upper [|0uy; 9uy; 10uy; 15uy; 255uy|])
+
     
