601663c65d820d919b5acb7daded92ef6dc09d8a
Idrek <iddarg@gmail.com>
2021-09-23 22:37:21 +0200

    Add tests for `objectChilds` function

--- test/AppTest/JsonTests.fs
+++ test/AppTest/JsonTests.fs
@@ -1,2 +1,31 @@
 module AppTest.JsonTests
 
+open App.Json
+open System.Text.Json
+open Xunit
+
+[<Fact>]
+let ``Tests for objectChilds function`` () =
+    let json : string = """
+        {
+            "en_GB": [
+                {
+                  "md5": "fd632797a9adb10d068868f86a2b6951", 
+                  "name": "keno.html", 
+                  "required": true
+                }
+            ],
+            "skin": "black"
+        }
+    """
+    let root : JsonElement = JsonDocument.Parse(json).RootElement
+    match objectChilds root |> Result.map Seq.toArray with
+    | Error e -> failwith "Invalid branch"
+    | Ok props ->
+        Assert.Equal("en_GB", props.[0].Name)
+        Assert.Equal(JsonValueKind.Array, props.[0].Value.ValueKind)
+        Assert.Equal("skin", props.[1].Name)
+        Assert.Equal(JsonValueKind.String, props.[1].Value.ValueKind)
+        Assert.Equal("black", props.[1].Value.GetString())
+
+        
