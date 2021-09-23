8b42e69a7c63d8c67937fda0d239a13a4db0aaa2
Idrek <iddarg@gmail.com>
2021-09-23 13:42:40 +0200

    New `Tree.ToPaths` method

--- src/App/Assets.fs
+++ src/App/Assets.fs
@@ -1,10 +1,14 @@
 namespace App.Assets
 
+module FS = App.FileSystem
 module Json = App.Json
 
 type JsonDocument = System.Text.Json.JsonDocument
 type JsonElement = System.Text.Json.JsonElement
 type JsonProperty = System.Text.Json.JsonProperty
+type Queue<'t> = System.Collections.Generic.Queue<'t>
+type Stack<'t> = System.Collections.Generic.Stack<'t>
+type String = System.String
 
 type File = {
     Md5: string
@@ -55,4 +59,26 @@ with
                 if isError 
                 then Result.Error "Error: Parse JSON to Assets type"
                 else Ok <| Dir (".", assets))
+
+    static member ToPaths (assets: Tree) : array<string * string> =
+            let pathSegments : Stack<string> = new Stack<string>()
+            let filePaths : Queue<string * string> = new Queue<string * string>()
+            let rec loop assets =
+                match assets with
+                | Dir (dir, childs) ->
+                    pathSegments.Push dir
+                    if Array.isEmpty childs
+                    then 
+                        let dirPath : string = FS.toPath pathSegments None
+                        filePaths.Enqueue (String.Empty, dirPath)
+                    else ()
+                    childs |> Array.iter loop
+                    pathSegments.Pop() |> ignore
+                | File { Md5 = md5; Name = name; Required = true } ->
+                    let filePath : string = FS.toPath pathSegments (Some name)
+                    filePaths.Enqueue (md5, filePath)
+                | _ -> ()
+            loop assets
+            filePaths.ToArray()
+
 
