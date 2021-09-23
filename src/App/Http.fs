84d77b254e224ff27babc8550e9ff1579a3e556a
Idrek <iddarg@gmail.com>
2021-09-23 12:52:53 +0200

    New `createRequest` function

--- src/App/Http.fs
+++ src/App/Http.fs
@@ -1,1 +1,10 @@
 module App.Http
+
+module C = HttpFs.Client
+module MRequest = C.Request
+
+type TRequest = C.Request
+
+let createRequest (uri: string) : TRequest =
+    MRequest.createUrl (C.HttpMethod.Get) uri
+    
