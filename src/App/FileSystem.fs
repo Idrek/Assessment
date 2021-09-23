84added0e02969c9624a07efa8e6a8ed1e9c1eaf
Idrek <iddarg@gmail.com>
2021-09-23 11:34:23 +0200

    New `toPath` function

--- src/App/FileSystem.fs
+++ src/App/FileSystem.fs
@@ -3,6 +3,7 @@ module App.FileSystem
 type Directory = System.IO.Directory
 type DirectoryInfo = System.IO.DirectoryInfo
 type Path = System.IO.Path
+type Stack<'t> = System.Collections.Generic.Stack<'t>
 
 let createDir (path: string) : Option<string> =
     try Directory.CreateDirectory path |> ignore; None
@@ -17,4 +18,12 @@ let buildFullPath (baseDir: string) (path: string) : Result<string, string> =
     try
         Ok <| DirectoryInfo(Path.Join(baseDir, path)).FullName
     with | ex -> Result.Error ex.Message
+
+let toPath (segments: Stack<string>) (fileNameO: Option<string>) : string =
+    let fileSegment : array<string> =
+        match fileNameO with
+        | None -> Array.empty
+        | Some fileName -> Array.singleton fileName
+    let path : array<string> = Array.append (segments.ToArray() |> Array.rev) fileSegment
+    Path.Combine(path)
     
