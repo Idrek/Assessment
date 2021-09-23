74738598990f36cba551503e2292d38e95febc59
Idrek <iddarg@gmail.com>
2021-09-23 11:32:57 +0200

    New `isDir` function

--- src/App/Json.fs
+++ src/App/Json.fs
@@ -2,6 +2,7 @@ module App.Json
 
 type JsonElement = System.Text.Json.JsonElement
 type JsonProperty = System.Text.Json.JsonProperty
+type JsonValueKind = System.Text.Json.JsonValueKind
 
 let arrayChilds (property: JsonProperty) : Result<seq<JsonElement>, string> =
     try
@@ -12,4 +13,10 @@ let objectChilds (item: JsonElement) : Result<seq<JsonProperty>, string> =
     try
         item.EnumerateObject() |> Seq.cast<JsonProperty> |> Ok
     with | ex -> Result.Error ex.Message
-    
+
+let isDir (item: JsonElement) : bool =
+    match objectChilds item with
+    | Error e -> false
+    | Ok properties -> 
+        properties |> Seq.head |> fun property -> property.Value.ValueKind = JsonValueKind.Array
+        
