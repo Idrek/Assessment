b5a8622e64fee645adc787869528b1d992dc3e7b
Idrek <iddarg@gmail.com>
2021-09-23 21:31:26 +0200

    New `App.Run` method

--- src/App/App.fs
+++ src/App/App.fs
@@ -1,10 +1,26 @@
 namespace App
 
+open CommandLine
 
 module Cryptography = App.Cryptography
 module FS = App.FileSystem
+module Http = App.Http
+module Json = App.Json
+
+type File = System.IO.File
 type IDownloable = App.Assets.IDownloable
+type JsonDocument = System.Text.Json.JsonDocument
 type Path = System.IO.Path
+type Tree = App.Assets.Tree
+type Uri = System.Uri
+
+type Options = {
+    [<Option("uri", Required=true, HelpText="Uri from where download files")>]
+    Uri: string
+    [<Option("dir", Required=true, HelpText="Directory to save files")>]
+    Dir: string
+}
+
 type App (asset: IDownloable) =
 
     let downloadFiles (baseDir: string) (baseUri: string) (paths: array<string * string>) =
@@ -28,5 +44,22 @@ type App (asset: IDownloable) =
                             let! _ = asset.Download fileUri fullPath
                             ()
                         | _ -> ()
+        }
+
+    member this.Run (options: Options) =
+        async {
+            let { Uri = baseUri; Dir = baseDir } : Options = options
+            let assetsFileName : string = "assetsTree.json"
+            let assetsUri : string = Path.Join(baseUri, assetsFileName)
+            let! (assetsStructure : Result<string, string>) = Http.download assetsUri
+            let (assetsR : Result<JsonDocument, string>) = Result.bind Json.ofString assetsStructure
+            match assetsR with
+            | Error message -> failwith message
+            | Ok assets ->
+                let pathsR : Result<array<string * string>, string> = 
+                    Tree.FromJson assets |> Result.map Tree.ToPaths
+                match pathsR with
+                | Error message -> failwith message
+                | Ok paths -> do! downloadFiles baseDir baseUri paths
         }
 
