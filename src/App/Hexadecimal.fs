1cbcb35f89cb8aaf2847d64e691253898a7e120b
Idrek <iddarg@gmail.com>
2021-09-23 11:24:49 +0200

    New `fromByte` function

--- src/App/Hexadecimal.fs
+++ src/App/Hexadecimal.fs
@@ -1,1 +1,12 @@
 module App.Hexadecimal
+
+type String = System.String
+
+type Case = Lower | Upper
+
+let fromByte (case: Case) (b: byte) : string =
+    let hexFormat : string =
+        match case with
+        | Lower -> "{0:x2}"
+        | Upper -> "{0:X2}"
+    String.Format(hexFormat, b)
