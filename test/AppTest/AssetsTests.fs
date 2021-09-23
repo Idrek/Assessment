dd158fc32d493693586238591e845a2d3cbc9e01
Idrek <iddarg@gmail.com>
2021-09-23 22:55:02 +0200

    Add tests for `Assets.Download` method

--- test/AppTest/AssetsTests.fs
+++ test/AppTest/AssetsTests.fs
@@ -3,9 +3,12 @@ module AppTest.AssetsTests
 open App.Assets
 open Microsoft.Extensions.DependencyInjection
 open System.Text.Json
+open System.Threading.Tasks
 open Xunit
 
+type Thread = System.Threading.Thread
 type IServiceProvider = System.IServiceProvider
+type Path = System.IO.Path
 
 let provider () : IServiceProvider =
     let services : ServiceCollection = ServiceCollection()
@@ -100,4 +103,27 @@ let ``Tests for Tree.ToPaths method`` () =
     |]
     Assert.Equal<array<string * string>>(result, Tree.ToPaths assets)
 
-    
+[<Fact>]
+let ``Tests for Assets.Download method`` () =
+    let t = new Task(fun () -> WebServer.App.main Array.empty |> ignore)
+    t.Start()
+    // Some time for the web server to start.
+    Thread.Sleep 1000
+    async {
+        let httpClientFactory : IHttpClientFactory = provider().GetRequiredService<IHttpClientFactory>()
+        let assets : IDownloable = Assets(httpClientFactory) :> IDownloable
+        let fileName : string = Path.GetTempFileName ()
+        let! result = assets.Download "http://localhost:5000/partial.txt" fileName
+        match result with
+        | Error e -> failwithf "Invalid branch: %A" e
+        | Ok () ->
+            let fileData : string = File.ReadAllText fileName
+            let output : string = 
+                """monday
+tuesday
+wednesday
+thursday
+fri"""
+            Assert.Equal(output, fileData)
+            File.Delete fileName
+    }
