7422fa9d24f63d1674666cced39b584c2097d0bd
Idrek <iddarg@gmail.com>
2021-09-23 11:29:12 +0200

    New `checkMd5` function

--- src/App/Cryptography.fs
+++ src/App/Cryptography.fs
@@ -1,1 +1,15 @@
 module App.Cryptography
+
+module Hex = App.Hexadecimal
+
+type File = System.IO.File
+type MD5 = System.Security.Cryptography.MD5
+
+let checkMd5 (md5hash: string) (path: string) : bool =
+    if File.Exists path then
+        let bytes : array<byte> = File.ReadAllBytes path
+        use md5 : MD5 = MD5.Create()
+        let res = md5.ComputeHash bytes |> Hex.fromBytes Hex.Case.Lower
+        res = md5hash
+    else false
+    
