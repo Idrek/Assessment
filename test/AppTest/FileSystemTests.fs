fefa4ae18a52945809697148c9526803cab28bb2
Idrek <iddarg@gmail.com>
2021-09-23 23:09:53 +0200

    Add tests for `toPath` function

--- test/AppTest/FileSystemTests.fs
+++ test/AppTest/FileSystemTests.fs
@@ -1,2 +1,22 @@
 module AppTest.FileSystemTests
 
+open App.FileSystem
+open Xunit
+
+type Stack<'t> = System.Collections.Generic.Stack<'t>
+type String = System.String
+
+[<Fact>]
+let ``Tests for toPath function`` () =
+    let segments : Stack<string> = new Stack<String>(Array.empty)
+    Assert.Equal(String.Empty, toPath segments None)
+    Assert.Equal("file1", toPath segments (Some "file1"))
+
+    let segments : Stack<string> = new Stack<String>([|"dir1"|])
+    Assert.Equal("dir1/file1", toPath segments (Some "file1"))
+
+    let segments : Stack<string> = new Stack<String>([|"dir1"; "dir2"|])
+    Assert.Equal("dir1/dir2/file1", toPath segments (Some "file1"))
+    Assert.Equal("dir1/dir2", toPath segments None)
+
+    
