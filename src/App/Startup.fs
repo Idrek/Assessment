cef1986087793d104505f01d29da35cdff9ca3ac
Idrek <iddarg@gmail.com>
2021-09-23 21:36:23 +0200

    New `createHostBuilder` function

--- src/App/Startup.fs
+++ src/App/Startup.fs
@@ -1,2 +1,17 @@
 module App.Startup
 
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+type App = App.App
+type Assets = App.Assets.Assets
+type IDownloable = App.Assets.IDownloable
+
+let createHostBuilder (args: array<string>) : IHostBuilder =
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices(fun (context: HostBuilderContext) (services: IServiceCollection) ->
+            services.AddHttpClient()
+                .AddScoped<App>()
+                .AddScoped<IDownloable, Assets>() |> ignore)
+
+                
