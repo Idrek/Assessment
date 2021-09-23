e273bd2cda2df0f9499e7d56941fc28a2577b897
Idrek <iddarg@gmail.com>
2021-09-23 10:38:21 +0200

    New `ensureDir` function

--- src/App/FileSystem.fs
+++ src/App/FileSystem.fs
@@ -5,4 +5,9 @@ type Directory = System.IO.Directory
 let createDir (path: string) : Option<string> =
     try Directory.CreateDirectory path |> ignore; None
     with | ex -> Some ex.Message
+
+let ensureDir (path: string) : Option<string> =
+    if Directory.Exists path
+    then None
+    else createDir path
     
