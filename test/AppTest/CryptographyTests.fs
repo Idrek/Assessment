47dfaed533985956458db490b031f7975edf1633
Idrek <iddarg@gmail.com>
2021-09-24 10:46:46 +0200

    Add tests for `checkMd5` function

--- test/AppTest/CryptographyTests.fs
+++ test/AppTest/CryptographyTests.fs
@@ -1,2 +1,24 @@
 module AppTest.CryptographyTests
 
+open App.Cryptography
+open Xunit
+
+type File = System.IO.File
+type Path = System.IO.Path
+
+[<Fact>]
+let ``Tests for checkMd5 function`` () =
+    let checkSum : string = "edfc80bdc5c752b6a73543dbba07d3ff"
+    let contents : string = 
+        """monday
+tuesday
+wednesday
+thursday
+friday
+saturday
+sunday"""
+    let fileName : string = Path.GetTempFileName ()
+    File.WriteAllText(fileName, contents)
+    Assert.True(checkMd5 checkSum fileName)
+
+    
